302180daa140fe4c046de4cf1a6788da0ec46530
Srinivas Pendem <pendem.svs@gmail.com>
2020-05-12 19:46:56 +0530

    corrected little bit of the errors.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -38,78 +38,78 @@ elif(InputA==set([3,5,7])):
 else:
     b = input("player 2, Enter your input:")
     InputB.add(b)
-    if(InputB==set(1,2,3)):
+    if(InputB==set([1,2,3])):
         print("player 2 win")
-    elif(InputB==set(4,5,6)):
+    elif(InputB==set([4,5,6])):
         print("player 2 win")
-    elif(InputB==set(7,8,9)):
+    elif(InputB==set([7,8,9])):
         print("player 2 win")
-    elif(InputB==set(1,4,7)):
+    elif(InputB==set([1,4,7])):
         print("player 2 win")
-    elif(InputB==set(2,5,8)):
+    elif(InputB==set([2,5,8])):
         print("player 2 win")
-    elif(InputB==set(3,6,9)):
+    elif(InputB==set([3,6,9])):
         print("player 2 win")
-    elif(InputB==set(1,5,9)):
+    elif(InputB==set([1,5,9])):
         print("player 2 win")
-    elif(InputB==set(3,5,7)):
+    elif(InputB==set([3,5,7])):
         print("player 2 win")
     else:
         a = input("player 1, Enter your input:")
         InputA.add(a)
-        if(InputA==set(1,2,3)):
+        if(InputA==set([1,2,3])):
             print("player 1 win")
-        elif(InputA==set(4,5,6)):
+        elif(InputA==set([4,5,6])):
             print("player 1 win")
-        elif(InputA==set(7,8,9)):
+        elif(InputA==set([7,8,9])):
             print("player 1 win")
-        elif(InputA==set(1,4,7)):
+        elif(InputA==set([1,4,7])):
             print("player 1 win")
-        elif(InputA==set(2,5,8)):
+        elif(InputA==set([2,5,8])):
             print("player 1 win")
-        elif(InputA==set(3,6,9)):
+        elif(InputA==set([3,6,9])):
             print("player 1 win")
-        elif(InputA==set(1,5,9)):
+        elif(InputA==set([1,5,9])):
             print("player 1 win")
-        elif(InputA==set(3,5,7)):
+        elif(InputA==set([3,5,7])):
             print("player 1 win")
         else:
             b = input("player 2, Enter your input:")
             InputB.add(b)
-            if(InputB==set(1,2,3)):
+            if(InputB==set([1,2,3])):
                 print("player 2 win")
-            elif(InputB==set(4,5,6)):
+            elif(InputB==set([4,5,6])):
                 print("player 2 win")
-            elif(InputB==set(7,8,9)):
+            elif(InputB==set([7,8,9])):
                 print("player 2 win")
-            elif(InputB==set(1,4,7)):
+            elif(InputB==set([1,4,7])):
                 print("player 2 win")
-            elif(InputB==set(2,5,8)):
+            elif(InputB==set([2,5,8])):
                 print("player 2 win")
-            elif(InputB==set(3,6,9)):
+            elif(InputB==set([3,6,9])):
                 print("player 2 win")
-            elif(InputB==set(1,5,9)):
+            elif(InputB==set([1,5,9])):
                 print("player 2 win")
-            elif(InputB==set(3,5,7)):
+            elif(InputB==set([3,5,7])):
                 print("player 2 win")
             else:
                 a = input("player 1, Enter your input:")
                 InputA.add(a)
-                if(InputA==set(1,2,3)):
+                if(InputA==set([1,2,3])):
                     print("player 1 win")
-                elif(InputA==set(4,5,6)):
+                elif(InputA==set([4,5,6])):
                     print("player 1 win")
-                elif(InputA==set(7,8,9)):
+                elif(InputA==set([7,8,9])):
                     print("player 1 win")
-                elif(InputA==set(1,4,7)):
+                elif(InputA==set([1,4,7])):
                     print("player 1 win")
-                elif(InputA==set(2,5,8)):
+                elif(InputA==set([2,5,8])):
                     print("player 1 win")
-                elif(InputA==set(3,6,9)):
+                elif(InputA==set([3,6,9])):
                     print("player 1 win")
-                elif(InputA==set(1,5,9)):
+                elif(InputA==set([1,5,9])):
                     print("player 1 win")
-                elif(InputA==set(3,5,7)):
+                elif(InputA==set([3,5,7])):
                     print("player 1 win")
                 else:
                     print("DRAW")
