5ca5c7e3e4dc203881f0d31a69da4165c76f485b
Srinivas Pendem <pendem.svs@gmail.com>
2020-05-14 13:38:44 +0530

    *question done

--- array2.py
+++ array2.py
@@ -1,10 +1,5 @@
 num=int(input())
-row=0
-while row<num:
-    star= row+1
-    while star>0:
-        print("*",end= ' ')
-        star = star-1
-        row = row+1
-        
+for i in range (1, num+1):
+    for j in  range(1, i+1):
+        print("*",end="")
     print()
