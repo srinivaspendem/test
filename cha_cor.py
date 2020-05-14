17b50cf84bf8f5a60fa08c9c1dbd452befd69c86
Srinivas Pendem <pendem.svs@gmail.com>
2020-05-14 16:59:09 +0530

    changed variables to s1,s2,x1 and x2

--- cha_cor.py
+++ cha_cor.py
@@ -1,42 +1,42 @@
 def check(str1:str, str2:str)->bool:
-    i=0
-    ilen=0
+    s1=0
+    x1=0
     iend=str1.__len__()
-    j=0
-    jlen=0
+    s2=0
+    x2=0
     jend=str2.__len__()
-    while i<iend and j<jend:
-        if ilen==ilen:
-            if str1[i].isalpha() and str2[j].isalpha():
-                if str1[i]!=str2[j]:
+    while s1<iend and s2<jend:
+        if x1==x2:
+            if str1[s1].isalpha() and str2[s2].isalpha():
+                if str1[s1]!=str2[s2]:
                     return False
 
                 else:
-                    i+=1
-                    j+=1
-                    ilen+=1
-                    jlen+=1
+                    s1+=1
+                    s2+=1
+                    x1+=1
+                    x2+=1
 
             else:
-                if str1[i].isalpha()==False:
-                    ilen+=int(str1[i])
-                    i+=1
+                if str1[s1].isalpha()==False:
+                    x1+=int(str1[s1])
+                    s1+=1
 
-                if str2[j].isalpha()==False:
-                    ilen+=int(str2[j])
-                    i+=1
-        elif ilen<jlen:
-            if str1[i].isalpha():
-                ilen+=1
+                if str2[s2].isalpha()==False:
+                    x2+=int(str2[s2])
+                    s2+=1
+        elif x1<x2:
+            if str1[s1].isalpha():
+                x1+=1
             else:
-                ilen+=int(str1[i])
-            i+=1
+                x1+=int(str1[s1])
+            s1+=1
         else:
-            if str2[j].isalpha():
-                jlen+=1
+            if str2[s2].isalpha():
+                x2+=1
             else:
-                jlen+=int(str2[j])
-            j+=1
+                x2+=int(str2[s2])
+            s2+=1
     return True
 x=input("enter two strings :").split()
 print (check(x[0],x[1]))
