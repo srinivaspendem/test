d4e943903d8a847efa827eb7d4899378c4ea7e68
Srinivas Pendem <pendem.svs@gmail.com>
2020-05-14 11:21:37 +0530

    check may 14

---
+++ array.py
@@ -0,0 +1,6 @@
+friends = [1,0,2,3,0,4,5]
+friends.insert(1, "0")
+friends.insert(5, "0")
+friends.remove(5)
+friends.pop()
+print (friends)
---
+++ array2.py
@@ -0,0 +1,10 @@
+num=int(input())
+row=0
+while row<num:
+    star= row+1
+    while star>0:
+        print("*",end= ' ')
+        star = star-1
+        row = row+1
+        
+    print()
