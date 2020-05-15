c8d284098166609d85e8deaecf28567d266f6ccf
Srinivas Pendem <pendem.svs@gmail.com>
2020-05-15 11:28:31 +0530

    to remove duplicate from an array

---
+++ remove_duplicate.py
@@ -0,0 +1,11 @@
+#creat a pythin program to eliminate duplicate numbers
+#and to arrange them in assinding order
+def Remove(duplicate): 
+    final_list = [] 
+    for num in duplicate: 
+        if num not in final_list: 
+            final_list.append(num) 
+    return final_list 
+duplicate = [2, 4, 10, 20, 5,5,5,5,5,5,5,5,2, 20, 32,42,42,31,4]
+duplicate.sort() 
+print(Remove(duplicate))
