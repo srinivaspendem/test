f281e341f5f22e9fc27003a273a6c090b001e37d
Srinivas Pendem <pendem.svs@gmail.com>
2020-05-16 09:54:36 +0530

    check this

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,16 @@
-print("hello world")
+f= open("filename.ext", "r") # open the file in readonly mode
+lines = [] # create an empty list to store the lines in the file
+
+for line in f:
+    lines.append(line) # loop over the lines in the file and store them in the lines list
+
+for line in lines:
+    words_in_line = line.split() # create a list of the words in each line
+    print(word_in_line[0], words_in_line[-1]) # print out the 1st and last word of each line
+
+
+f = open("filename.ext", "r")
+
+for line in f:
+    words = line.split()
+    print(words[0], words[-1])
--- red_web.py
+++ red_web.py
@@ -4,4 +4,9 @@ link = "http://www.gutenberg.org/files/11/11-0.txt"
 
 f = urlopen(link)
 myfile = f.read()
-print(myfile)
+#word=myfile.readline().split()
+#print (myfile)
+#print(myfile.readline())
+word= myfile. readline().split
+#print(myfile)
+print(line)
